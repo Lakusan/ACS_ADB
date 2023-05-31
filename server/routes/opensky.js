90bdeb1091d225626c2cd4f42c7bac880cdf7a8e
Ugurcan Kaya <11018661@stud.hochschule-heidelberg.de>
2023-05-31 11:23:21 +0200

    filter by flight call sign

--- server/routes/opensky.js
+++ server/routes/opensky.js
@@ -1,61 +1,34 @@
 const router = require('express').Router();
 const axios = require('axios');
 
+// test
+router.get('/opensky/:flightNumber?', async (req, res) => {
+  const url = 'https://opensky-network.org/api/states/all';
 
+  const flightNumber = req.params.flightNumber || '';
 
+  axios
+    .get(url)
+    .then(response => {
+      const data = response.data;
 
+      // Extract flight information and filter by flight number if provided
+      const flights = data.states
+        .map(flight => ({
+          icao24: flight[0],
+          callsign: flight[1].replace(/\s/g, ''), // Remove spaces from the flight call number
+          originCountry: flight[2],
+          longitude: flight[5],
+          latitude: flight[6],
+          altitude: flight[7]
+        }))
+        .filter(flight => flightNumber === '' || flight.callsign === flightNumber);
 
-
-//test
-router.get('/opensky',  async (req,res) => {
-  
-    const url = 'https://opensky-network.org/api/states/all';
-
-    // Get optional parameters from user
-    const { lomin, lomax, lamin, lamax } = req.query;
-
-    // Default parameters for Europe
-    const defaultParams = {
-    lomin: -24,      // Minimum longitude for Europe
-    lomax: 40,       // Maximum longitude for Europe
-    lamin: 35,       // Minimum latitude for Europe
-    lamax: 71        // Maximum latitude for Europe
-    };
-
-    // Validate and use user-provided or default parameters
-    const parameters = {
-    lomin: validateCoordinate(lomin) ? parseFloat(lomin) : defaultParams.lomin,
-    lomax: validateCoordinate(lomax) ? parseFloat(lomax) : defaultParams.lomax,
-    lamin: validateCoordinate(lamin) ? parseFloat(lamin) : defaultParams.lamin,
-    lamax: validateCoordinate(lamax) ? parseFloat(lamax) : defaultParams.lamax
-    };
-
-    // Helper function to validate coordinate value
-    function validateCoordinate(coord) {
-    return coord && !isNaN(parseFloat(coord)) && isFinite(coord);
-    }
-
-    axios.get(url, { params: parameters })
-      .then(response => {
-        const data = response.data;
-  
-        // Extract flight information
-        const flights = data.states.map(flight => ({
-          icao24: flight[0],                    // Unique ICAO24 aircraft address
-          callsign: flight[1],                  // Callsign
-          originCountry: flight[2],             // Country of origin
-          longitude: flight[5],                 // Longitude
-          latitude: flight[6],                  // Latitude
-          altitude: flight[7]                   // Altitude in meters
-        }));
-  
-        res.json(flights);
-      })
-      .catch(error => {
-        res.status(500).json({ error: 'An error occurred while retrieving flight data' });
-      });
-   
-
+      res.json(flights);
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'An error occurred while retrieving flight data' });
+    });
 });
 
 module.exports = router;
