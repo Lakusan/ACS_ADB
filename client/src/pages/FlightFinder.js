47ef9579e26d0f26a04ddfc6247ff5c4d47f4491
sumathirongali <sumathirongali@gmail.com>
2023-06-04 20:26:33 +0200

    Updated chnages in frontend and backend to search flights from source to Destination

--- client/src/pages/FlightFinder.js
+++ client/src/pages/FlightFinder.js
@@ -1,63 +1,89 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 export function FlightFinder() {
   const [flightDetails, setFlightDetails] = useState([]);
+  const [source, setSource] = useState('');
+  const [destination, setDestination] = useState('');
+  const [date, setDate] = useState('');
 
-  useEffect(() => {
-    fetchAllFlightDetails();
-  }, []);
-
-  async function fetchFlightDetails(departureAirport, arrivalAirport) {
+  async function fetchFlightDetails() {
     try {
-      const response = await axios.post('http://localhost:3000/api/flights/Search', {departureAirport, arrivalAirport});
+      const response = await axios.post('http://localhost:3000/api/flights/search', {
+        departureAirport: source,
+        arrivalAirport: destination,
+        date: date
+      });
       setFlightDetails(response.data);
     } catch (error) {
       console.error('Error fetching flight details:', error);
     }
   }
 
-  async function fetchAllFlightDetails() {
-    try {
-      const response = await axios.get('http://localhost:3000/api/flights');
-      setFlightDetails(response.data);
-    } catch (error) {
-      console.error('Error fetching flight details:', error);
-    }
+  function calculateDuration(departureTime, arrivalTime) {
+    const start = new Date(departureTime);
+    const end = new Date(arrivalTime);
+    const durationInMs = end - start;
+    const durationInHours = durationInMs / (1000 * 60 * 60);
+    return durationInHours.toFixed(2);
+  }
+
+
+  function formatTime(time) {
+    return new Date(time).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
   }
 
   return (
     <div>
-      <h1>Flight Status Tracker</h1>
+      <h1>Flight Finder</h1>
 
       <form onSubmit={(e) => {
         e.preventDefault();
-        const departureAirport = e.target.elements.departureAirport.value;
-        const arrivalAirport = e.target.elements.arrivalAirport.value;
-        fetchFlightDetails(departureAirport, arrivalAirport);
+        fetchFlightDetails();
       }}>
+        <label htmlFor="source">Source:</label>
+        <input type="text" id="source" value={source} onChange={(e) => setSource(e.target.value)} required /><br /><br />
 
-        <label htmlFor="departureAirport">Departure Airport:</label>
-        <input type="text" id="departureAirport" name="departureAirport" required /><br /><br />
+        <label htmlFor="destination">Destination:</label>
+        <input type="text" id="destination" value={destination} onChange={(e) => setDestination(e.target.value)} required /><br /><br />
 
-        <label htmlFor="arrivalAirport">Arrival Airport:</label>
-        <input type="text" id="arrivalAirport" name="arrivalAirport" required /><br /><br />
+        <label htmlFor="date">Date:</label>
+        <input type="date" id="date" value={date} onChange={(e) => setDate(e.target.value)} required /><br /><br />
 
         <input type="submit" value="Search" />
       </form>
 
-      <button onClick={fetchAllFlightDetails}>Load Flight Details</button>
-
       <div id="flightDetails">
         {flightDetails.length === 0 ? (
-          <p>No flights found.</p>
+          source ==="" || destination ==="" || date === '' ? "" : <p>No flights found.</p>
         ) : (
-          flightDetails.map((flight, index) => (
-            <p key={index}>
-              Flight number: {flight.flightNumber}, Departure time: {flight.departureTime}, Arrival time: {flight.arrivalTime}
-            </p>
-          ))
-        )}
+          <table>
+          <thead>
+            <tr>
+              <th>Airline</th>
+              <th>FlightNumber</th>
+              <th>SourceCity</th>
+              <th>DestinationCity</th>
+              <th>DepartureTime</th>
+              <th>ArrivalTime</th>
+              <th>Duration</th>
+            </tr>
+          </thead>
+          <tbody>
+            {flightDetails.map((flight, index) => (
+              <tr key={index}>
+                <td>{flight.airline}</td>
+                <td>{flight.flightNumber}</td>
+                <td>{flight.departureCity}</td>
+                <td>{flight.arrivalCity}</td>
+                <td>{formatTime(flight.departureTime)}</td>
+                <td>{formatTime(flight.arrivalTime)}</td>
+                <td>{calculateDuration(flight.departureTime, flight.arrivalTime)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
       </div>
     </div>
   );
--- server/routes/flights.js
+++ server/routes/flights.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 // Import the FlightStatusReq model
 const FlightStatusReq = require('../models/FlightStatusReq');
+const Flight = require('../models/Flight');
 
 // GET route for fetching all flight details
 router.get('/flights', async (req, res) => {
   try {
-    const flights = await FlightStatusReq.find();
+    const flights = await Flight.find();
     res.json(flights);
   } catch (error) {
     console.error('Error querying flights:', error);
@@ -15,12 +16,13 @@ router.get('/flights', async (req, res) => {
 
 // POST route for fetching flight details based on search criteria
 router.post('/flights/search', async (req, res) => {
-  const { departureAirport, arrivalAirport } = req.body;
+  const { departureAirport, arrivalAirport, date } = req.body;
 
   try {
-    const flights = await FlightStatusReq.find({
+    const flights = await Flight.find({
       departureCity: departureAirport,
-      arrivalCity: arrivalAirport
+      arrivalCity: arrivalAirport,
+      operatingDays: { $in: [getDayOfWeek(date)] }
     });
 
     res.json(flights);
@@ -29,6 +31,13 @@ router.post('/flights/search', async (req, res) => {
     res.status(500).send('An error occurred');
   }
 });
+
+// Helper function to get the day of the week for a given date
+function getDayOfWeek(date) {
+  const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const dayIndex = new Date(date).getDay();
+  return daysOfWeek[dayIndex];
+}
 
 
 module.exports = router;
