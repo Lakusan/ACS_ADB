2f0f54cd55c551da2542c118d20106bba6ccdd47
Lakusan <Andreas.Lakus@googlemail.com>
2023-06-06 01:42:52 +0200

    server: added route to flightradar.js

--- server/routes/flightRadar.js
+++ server/routes/flightRadar.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
+const FlightDataRTPubService = require('../services/FlightDataRTPubService');
 
-//test
 router.get('/flights/radar', async (req, res) => {
-    var test = "TEST"
-    res.send(test);
+    const flightDataRTPubService = new FlightDataRTPubService("flight radar");
+    const dataJson = flightDataRTPubService.parseAPIData('states/all');
+    res.send(dataJson);
+    // res.send("DATA");
 });
 
 module.exports = router;
