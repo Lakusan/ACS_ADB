5126316830e0f62b73389d85a7b6aabe3d3e03fe
Lakusan <Andreas.Lakus@googlemail.com>
2023-06-07 01:45:22 +0200

    client: flightradar page now listens with effectHook for changes from socket on flight data

--- client/src/pages/FlightRadar.js
+++ client/src/pages/FlightRadar.js
@@ -1,26 +1,54 @@
-// import React from 'react';
-
-// export function FlightRadar() {
-//   return (
-//     <>
-//     <h1> FlightRadar Andreas </h1>
-//     </>
-//   );
-// }
-
-
-
+import React, { useEffect, useState } from 'react';
 import '../App.css';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-// import React, { Component } from 'react'
 import { Icon } from "leaflet";
-import FRDataFetcher from '../components/FRDataFetcher';
-
+// import FRDataFetcher from '../components/FRDataFetcher';
+// import useSocketSetup from '../services/useSocketSetup';
+import axios from 'axios';
+// import socket from '../services/socket';
+import { io } from "socket.io-client";
+//TODO: connect to socket on backen and retrieve data stream
+// parse data and spawn markers
 
 export function FlightRadar() {
+  // trigger subscription
+  axios.get('http://localhost:3000/api/flights/radar/subscribe');
+  // useSocketSetup();
 
+  const urlBackend = process.env.REACT_APP_SERVER_HOSTNAME + ":" + process.env.REACT_APP_SERVER_PORT;
 
+  const socket = new io(urlBackend, {
+    // transports: ["websocket"],
+    autoConnect: true
+  });
+  const [receivedData, setReceivedData] = useState('');
+  socket.on('connect', () => {
+          socket.on('send_message', (data) => {
+            // console.log(data);
+
+            setReceivedData(data);
+            console.log(JSON.parse(data));
+          });
+        });
+
+  
+    useEffect(() => {
+      // socket.on('connect', () => {
+      //   socket.on('send_message', (data) => {
+      //     console.log(data);
+
+      //     setReceivedData(data);
+      //   });
+      // });
+      // socket.on('connect_error', () => {
+      //   console.error("Socket connection error");
+      // });
+      // return () => {
+      //   socket.off("connect_error");
+      // }
+      console.log("data");
+    }, [receivedData]);
 
   return (
     <div>
@@ -31,11 +59,11 @@ export function FlightRadar() {
 }
 
 export function MapComponent() {
-  const srhLocation = 
-    {
-      geocode: [49.41386, 8.65164],
-      popUp: "SRH Heidelberg"
-    };
+  const srhLocation =
+  {
+    geocode: [49.41386, 8.65164],
+    popUp: "SRH Heidelberg"
+  };
 
   const srhIcon = new Icon({
     iconUrl: 'https://www.srh-hochschule-heidelberg.de/typo3conf/ext/site_srh_edu/Resources/Public/Favicons/apple-touch-icon-hochschule.png',
@@ -49,27 +77,28 @@ export function MapComponent() {
 
   return (
     <div className='App'>
-    <MapContainer center={[49.41386, 8.65164]} zoom={13} style={{ height: '400px' }}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution="Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors"
-      />
-      {
-        // markers.map((marker) => {
-        //   <Marker position={marker.geocode} icon={srhIcon}>
-        //     <Popup>
-        //       BLA
-        //     </Popup>
-        //   </Marker>
-        // })
-        <Marker position={srhLocation.geocode} icon={srhIcon}>
-          <Popup>
-            {srhLocation.popUp}
-          </Popup>
-        </Marker>
-      }
-    </MapContainer>
-    <FRDataFetcher></FRDataFetcher>
+      <MapContainer center={[49.41386, 8.65164]} zoom={13} style={{ height: '400px' }}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors"
+        />
+        {
+          markers.map((marker) => {
+            <Marker position={marker.geocode} icon={srhIcon}>
+              <Popup>
+                BLA
+              </Popup>
+            </Marker>
+          })
+          
+     
+        }
+             <Marker position={srhLocation.geocode} icon={srhIcon}>
+            <Popup>
+              {srhLocation.popUp}
+            </Popup>
+          </Marker>
+      </MapContainer>
     </div>
   );
 }
