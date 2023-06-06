11f6778369e9a21da20cd0582c859998fb9a033c
Lakusan <Andreas.Lakus@googlemail.com>
2023-06-07 01:39:14 +0200

    server: removed changes in test route

--- server/routes/test.js
+++ server/routes/test.js
@@ -2,10 +2,14 @@ const router = require('express').Router();
 
 //test
 router.get('/test',  async (req,res) => {
-   var test = "out"
+   var test = "out";
     res.send(test);
-   
-
 });
+
+router.get('/flight/search', async (req, res) => {
+    // query mongoDB
+    // compute Data 
+    res.send(data);
+})
 
 module.exports = router;
