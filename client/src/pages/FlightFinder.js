dfed87fff9d0753c8ac2c19bb417b2b6c3f8c0a7
sumathirongali <sumathirongali@gmail.com>
2023-06-08 22:18:52 +0200

    added css

--- client/src/pages/FlightFinder.js
+++ client/src/pages/FlightFinder.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import './FlightFinder.css';
 
 export function FlightFinder() {
   const [flightDetails, setFlightDetails] = useState([]);
@@ -28,44 +29,28 @@ export function FlightFinder() {
     return durationInHours.toFixed(2);
   }
 
-
   function formatTime(time) {
     return new Date(time).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
   }
 
-  return (
-    <div>
-      <h1>Flight Finder</h1>
-
-      <form onSubmit={(e) => {
-        e.preventDefault();
-        fetchFlightDetails();
-      }}>
-        <label htmlFor="source">Source:</label>
-        <input type="text" id="source" value={source} onChange={(e) => setSource(e.target.value)} required /><br /><br />
-
-        <label htmlFor="destination">Destination:</label>
-        <input type="text" id="destination" value={destination} onChange={(e) => setDestination(e.target.value)} required /><br /><br />
-
-        <label htmlFor="date">Date:</label>
-        <input type="date" id="date" value={date} onChange={(e) => setDate(e.target.value)} required /><br /><br />
-
-        <input type="submit" value="Search" />
-      </form>
-
-      <div id="flightDetails">
-        {flightDetails.length === 0 ? (
-          source ==="" || destination ==="" || date === '' ? "" : <p>No flights found.</p>
-        ) : (
-          <table>
+  function renderFlightDetails() {
+    if (flightDetails.length === 0) {
+      if (source === "" || destination === "" || date === "") {
+        return null;
+      } else {
+        return <p className="no-flights">No flights found.</p>;
+      }
+    } else {
+      return (
+        <table className="flight-table">
           <thead>
             <tr>
               <th>Airline</th>
-              <th>FlightNumber</th>
-              <th>SourceCity</th>
-              <th>DestinationCity</th>
-              <th>DepartureTime</th>
-              <th>ArrivalTime</th>
+              <th>Flight Number</th>
+              <th>Departure City</th>
+              <th>Arrival City</th>
+              <th>Departure Time</th>
+              <th>Arrival Time</th>
               <th>Duration</th>
             </tr>
           </thead>
@@ -83,7 +68,32 @@ export function FlightFinder() {
             ))}
           </tbody>
         </table>
-      )}
+      );
+    }
+  }
+
+  return (
+    <div className="flight-finder">
+      <h1 className="title">Flight Finder</h1>
+
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        fetchFlightDetails();
+      }}>
+        <label htmlFor="source">From:</label>
+        <input type="text" id="source" value={source} onChange={(e) => setSource(e.target.value)} required /><br /><br />
+
+        <label htmlFor="destination">To:</label>
+        <input type="text" id="destination" value={destination} onChange={(e) => setDestination(e.target.value)} required /><br /><br />
+
+        <label htmlFor="date">Date:</label>
+        <input type="date" id="date" value={date} onChange={(e) => setDate(e.target.value)} required /><br /><br />
+
+        <input type="submit" value="Search" className="submit-button" />
+      </form>
+
+      <div className="flight-details">
+        {renderFlightDetails()}
       </div>
     </div>
   );
