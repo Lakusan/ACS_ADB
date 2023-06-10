612dc4764cdd865a30ce7552304f26acbc9199dc
Lakusan <Andreas.Lakus@googlemail.com>
2023-06-10 21:03:00 +0200

    server: serverjs instancate services, remove old functions

--- server/server.js
+++ server/server.js
@@ -7,24 +7,25 @@ const {Server} = require('socket.io');
 
 // DB imports
 const neo4j = require('neo4j-driver');
-const redis = require('redis');
 const mongoose = require('mongoose');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: '*',
-    methods: ["GET", "POST"]
-  }
-});
 
+// socket.io server config
+// const io = new Server(server, {
+//   cors: {
+//     origin: '*',
+//     methods: ["GET", "POST"]
+//   }
+// });
 // Services 
 const FlightDataRTPubService = require('./services/FlightDataRTPubService');
-const flightDataRTSubService = require('./services/FlightDataRTSubService');
-const redisServices = require('./services/redisServices');
+const FlightDataRTSubService = require('./services/FlightDataRTSubService');
+const IO = require('./services/SocketServer');
+const EventListener = require('./services/EventListener');
 
-//Configuration
+// nodjs server configuration
 dotenv.config();
 const SERVER_PORT = process.env.SERVER_PORT;
 
@@ -35,6 +36,7 @@ const flightRadar = require('./routes/flightRadar');
 
 //Middleware
 app.use(express.json());
+// cors allow origin all
 app.use(cors({
   origin: '*',
 }));
@@ -47,7 +49,7 @@ app.use('/api', flightRadar);
 //Connect to MongoDB
 mongoose.connect(process.env.MONGODB_CONNECT, {
   useNewUrlParser: true,
-
+  
   useUnifiedTopology: true,
 });
 const db = mongoose.connection;
@@ -62,33 +64,43 @@ const neo4jDriver = neo4j.driver(process.env.NEO4J_CONNECT, neo4j.auth.basic(pro
 const neo4jSession = neo4jDriver.session();
 
 neo4jDriver.verifyConnectivity()
-  .then(() => {
-    console.log('Connected to Neo4j');
-  })
-  .catch((error) => {
-    console.error('Neo4j connection error:', error);
-  });
+.then(() => {
+  console.log('Connected to Neo4j');
+})
+.catch((error) => {
+  console.error('Neo4j connection error:', error);
+});
 
 //Listen Server
 server.listen(SERVER_PORT, () => console.log(`Server is running on PORT: ${SERVER_PORT}`));
 //docker run -d -p 6379:6379 --name myredis --network redisnet redis
 
+// attach socket.io server to app to get access to this instance from other modules
+// app.set('socketio', io);
+
+// start socket server
+const io = IO.getInstance();
+io.initialize(server);
 // Start Services for Flight Radar
-const flightDataRTPubService = new FlightDataRTPubService("flight radar");
+// gets data from API and publishes it on redis pub/sub channel -> ENV: REDIS_PUB_FLIGHT_RADAR
+const flightDataRTPubService = FlightDataRTPubService.getInstance();
+// subscribes to redis pub sub subscriber and gets data from topic.
+const flightDataRTSubService = FlightDataRTSubService.getInstance();
+const eventListener = new EventListener(flightDataRTSubService, flightDataRTPubService, io);
+// Eventlistener: on message in redis channel triggers socket emit with data
 flightDataRTPubService.collectRTDataFromAPI();
+flightDataRTSubService.connect(process.env.REDIS_PUB_FLIGHT_RADAR);
+
+
+
+
 //Socket IO
-// WORKS
-io.on("connection", (socket) => {
-  console.log(`User Connected: ${socket.id}`);
-  socket.on("send_message", (data) => {
-  console.log(data);
-  });
-});
-//  app.io = io;
-// app.use( function ( req, res, next){
-//   req.io = io;
-//   next();
-// })
-app.set('socketio', io);
+  
+//   io.on("connection", (socket) => {
+//     console.log(`User Connected: ${socket.id}`);
+//     socket.on("send_message", (data) => {
+//     console.log(data);
+//   });
+// });
 
 module.exports.app = app;
