b5a6ee903f321d87e4521ecd3db39811e38ee94b
Lakusan <Andreas.Lakus@googlemail.com>
2023-06-12 10:51:22 +0200

    server: refactoring, server/redisClient.js removed

--- server/server.js
+++ server/server.js
@@ -1,23 +1,15 @@
 //Import Libraries
 const express = require('express');
 const cors = require('cors');
-
 const dotenv = require('dotenv');
-
 const neo4j = require('neo4j-driver');
 const redis = require('redis');
 const mongoose = require('mongoose');
-const redisClient = require('./redisClient');
-
-
-
 
 //Configuration
 const app = express();
 dotenv.config();
 const SERVER_PORT = process.env.SERVER_PORT;
-
-
 
 //Import Routes
 const testRoute = require('./routes/test');
@@ -26,12 +18,10 @@ const flightStatus = require('./routes/flights');
 const airports = require('./routes/airports');
 const route = require('./routes/route');
 
-
 //Middleware
 app.use(express.json());
 app.use(cors({
-    origin: '*',
-    
+    origin: '*'
 }));
 
 //Route Middlewares
@@ -40,7 +30,6 @@ app.use('/api', opensky);
 app.use('/api', flightStatus);
 app.use('/api/', airports);
 app.use('/api/', route);
-
 
 //Connect to MongoDB
 mongoose.connect(process.env.MONGODB_CONNECT, {
@@ -54,7 +43,6 @@ db.once('open', () => {
   console.log('Connected to MongoDB');
 });
 
-
 //Connect to neo4J
 const neo4jDriver = neo4j.driver(process.env.NEO4J_CONNECT, neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD));
 const neo4jSession = neo4jDriver.session();
@@ -67,9 +55,7 @@ neo4jDriver.verifyConnectivity()
     console.error('Neo4j connection error:', error);
   });
 
-
 //Listen Server
 app.listen(SERVER_PORT, () => console.log(`Server is running on PORT: ${SERVER_PORT}`));
 //docker run -d -p 6379:6379 --name myredis --network redisnet redis
 module.exports.app = app;
-
