a539b7b18889929e7594c4700d2db938efa038d8
Lakusan <Andreas.Lakus@googlemail.com>
2023-06-12 10:55:16 +0200

    server: route opensky Bugfix Redis connection error -> source: 2 calls of api quit connection 2x

--- server/routes/opensky.js
+++ server/routes/opensky.js
@@ -8,20 +8,15 @@ const url = `redis://${process.env.REDIS_HOSTNAME}:${process.env.REDIS_PORT}`;
 const redisClient = redis.createClient({
   url
 });
-const redisClientListen = redis.createClient({
-  url
-});
-
-
 
 //Functions to get data from mongodb (airport info)
 //get airport info
 
 async function getAirportInformation(iataCode) {
   const client = new MongoClient(process.env.MONGODB_CONNECT, { useUnifiedTopology: true });
+  client.connect();
 
   try {
-    await client.connect();
 
     const db = client.db(process.env.MONGODB_DB_NAME);
     const airportsCollection = db.collection('airports');
@@ -30,8 +25,6 @@ async function getAirportInformation(iataCode) {
     if (!airportInfo) {
       throw new Error('Airport not found');
     }
-
-  
 
     const airportWithInformation = {
       name: airportInfo.name,
@@ -43,48 +36,48 @@ async function getAirportInformation(iataCode) {
 
     return airportWithInformation;
   } finally {
+    console.log("--> OPENSKY: Close MongoDB");
     client.close();
   }
 }
 
 
-
-
-
-
 router.get('/opensky/flights/:iata', async (req, res) => {
   const iata = req.params.iata.toLowerCase();
-
-
-   //check if data exists in redis
-   redisClientListen.connect();
-   redisClientListen.on('error', err => console.log('Redis error: ', err.message));
-   const value = await redisClientListen.get(`flight_info:${iata}`);
-   if (value) {
-     const cachedData = JSON.parse(value);
-     res.status(200).json({data: cachedData , source: 'redis'});
-     redisClientListen.quit();
-     return;
-   }
-   redisClientListen.quit();
-   
-
+  console.log("/opensky/flights/:iata");
+  
+  if (redisClient.isOpen){
+    console.log("REDIS IS OPEN")
+  } else {
+    redisClient.connect();
+    redisClient.on('connection', () => console.log('--> OPENSKY:; Connected to REDIS'));
+  }
+  //check if data exists in redis
+  redisClient.on('error', err => console.log('Redis error: ', err.message));
+  const value = await redisClient.get(`flight_info:${iata}`);
+  if (value) {
+    const cachedData = JSON.parse(value);
+    // redisClient.quit();
+    console.log('--> OPENSKY: REDIS Disconnect checking data');
+    res.status(200).json({ data: cachedData, source: 'redis' });
+    return;
+  }
 
   try {
+    console.log('--> OPENSKY: GETTING DATA FROM API');
     const response = await axios.get(`http://api.aviationstack.com/v1/flights`, {
       params: {
         access_key: process.env.AVISTACK_API_KEY,
         flight_iata: iata
-
       }
     });
 
-    redisClient.connect();
-    redisClient.setEx(`flight_info:${iata}`, 60*15, JSON.stringify(response.data.data)); // Cache for 15 min    
-    redisClient.quit();
+    redisClient.setEx(`flight_info:${iata}`, 60 * 15, JSON.stringify(response.data.data)); // Cache for 15 min    
+    // redisClient.quit();
+    console.log('--> OPENSKY: REDIS Disconnect at the end');
 
 
-    res.json({data: response.data.data, source: "api"});
+    res.json({ data: response.data.data, source: "api" });
 
 
   } catch (error) {
@@ -95,6 +88,7 @@ router.get('/opensky/flights/:iata', async (req, res) => {
 
 
 router.get('/opensky/airportInfo/:iata', async (req, res) => {
+  console.log("/opensky/airportInfo/:iata");
 try{
 
   const iata = req.params.iata.toUpperCase();
@@ -115,8 +109,6 @@ try{
   console.error('Error retrieving airport data:', error);
   res.status(500).json({ error: 'An error occurred while retrieving airport data' });
 }
-
-
 });
 
 
