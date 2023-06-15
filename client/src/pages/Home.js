c436d27ae78bddae5a4df92a80b641abb99eb5c6
Lakusan <Andreas.Lakus@googlemail.com>
2023-06-15 12:03:59 +0200

    client: Homepage styling

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import image from '../resources/pexels-andrea-piacquadio-837129.jpg'
+//https://www.pexels.com/photo/man-in-brown-robe-carrying-bag-smiling-837129/
+import '../App.css';
+
+
 
 export function Home() {
   return (
     <>
-    <h1> Homepage </h1>
+      <h1 class="centered-h1"> Flight Information System </h1>
+      <div class="image-container">
+        <img src={image} alt="title image source: https://www.pexels.com/photo/man-in-brown-robe-carrying-bag-smiling-837129/" className="centered-image"/>
+      </div>
     </>
   );
 }
