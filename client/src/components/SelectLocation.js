d75d915999a1ce8c74d6356d61bf36902343c614
Lakusan <Andreas.Lakus@googlemail.com>
2023-06-15 13:37:08 +0200

    refactoring

--- client/src/components/SelectLocation.js
+++ client/src/components/SelectLocation.js
@@ -1,46 +1,46 @@
 import React, { useEffect } from 'react'
 import { Dropdown } from 'semantic-ui-react'
 
- 
+
 
 let cityOptions = [
-    { key: 'NA', value: 'NA', text: 'Not available' },
+    { key: 'NA', value: 'NA', text: 'Not available' },
 ]
 
- 
+
 
 const SelectLocation = (props) => {
-    useEffect(() => {
-        if (props.airportData) {
-            const cities = props.airportData.map((item) => {
-                return {
-                    key: item.IATA,
-                    value: item.IATA,
-                    text: `${item.City} (${item.IATA})`
-                }
-            })
-            cityOptions = cities
-        }
-    }, [props.airportData])
+    useEffect(() => {
+        if (props.airportData) {
+            const cities = props.airportData.map((item) => {
+                return {
+                    key: item.IATA,
+                    value: item.IATA,
+                    text: `${item.City} (${item.IATA})`
+                }
+            })
+            cityOptions = cities
+        }
+    }, [props.airportData])
 
- 
 
-    const onValueChange = (e, { value }) => props.sendVal(value)
 
- 
+    const onValueChange = (e, { value }) => props.sendVal(value)
 
-    return (
-<Dropdown
-            placeholder='Select City'
-            fluid
-            search
-            selection
-            options={cityOptions}
-            onChange={onValueChange}
-        />
-    )
+
+
+    return (
+        <Dropdown
+            placeholder='Select City'
+            fluid
+            search
+            selection
+            options={cityOptions}
+            onChange={onValueChange}
+        />
+    )
 }
 
- 
+
 
 export default SelectLocation
